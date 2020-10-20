3d3b22ceaf1a3e15dd597776e880c497d2a76984
xudp <xudp@prttech.com>
2020-10-20 08:40:06 +0800

    Added:Update demo of lib

--- demo/demo_server.c
+++ demo/demo_server.c
@@ -3,35 +3,59 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <pthread.h>
 #include "lib_list.h"
 #include "lib_net.h"
+#include "lib_debug.h"
 
 void Server_Data_Handle(LIB_NET_CONNECT_T *conn, char *data, unsigned short datalen)
 {
 	int i = 0;
+
 	if (NULL == conn)
 		return;
 
-	printf("[%s:%d] The client(fd:%d, IP:%s, Port:%d) recv data(%d):", __FUNCTION__, __LINE__, conn->fd, conn->addr, conn->port, datalen);
+	DBG_PRINT(1, "The client(fd:%d, IP:%s, Port:%d) recv data(%d):", conn->fd, conn->addr, conn->port, datalen);
 	for (;i < datalen; i++) {
 		printf("0x%02x ", data[i]);
 	}
 	printf("\n");
+	Net_Data_Send(conn->fd, data, datalen);
+}
+
+/*******************************************************************************************************************
+**	函数名:Net_Server_Thread
+**	描	述:服务器连接线程
+**	参	数:[in]param:服务器节点参数
+**	返回值:无
+********************************************************************************************************************/
+static void *Demo_Net_Server_Thread(void* param)
+{
+	LIB_NET_SERVER_T server;
+
+	server.port = 9200;
+	server.connect_max = 20;
+	server.Net_Server_Data_Handle = Server_Data_Handle;
+
+	Net_Creat_Server(&server);
+	DBG_PRINT(1, "Exit server\n");
+	return NULL;
 }
 
 int main (void)
 {
-	LIST_T *list_server;
+	DEBUG_INFO debug;
+	pthread_t DemoThreadServer;
 
-	list_server = (LIST_T *)malloc(sizeof(LIST_T));
-	memset(list_server, 0x0, sizeof(LIST_T));
-	printf("[%s:%d] creat server, port is 9200, max connect num is 20\n", __FUNCTION__, __LINE__);
-	Net_Creat_Server(&list_server, 9200, 20, Server_Data_Handle);
-
+	debug.InterfaceType = 1 << INTERFACE_TYPE_CONSOLE;
+	Debug_Init(&debug);
+	if (pthread_create(&DemoThreadServer, NULL, Demo_Net_Server_Thread, NULL) != 0) {
+        DBG_PRINT(1, "pthread_create Demo_Net_Server_Thread failed.\n");
+    }
+	
 	while(1) {
 		sleep(2);
 	}
-	free(list_server);
+	
 	return 1;
 }
